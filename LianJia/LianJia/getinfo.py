fd6a79b968aba09d9788acbbc500de9faf1a4291
cwd0204 <cwd0204@users.noreply.github.com>
2021-01-21 20:32:10 +0800

    check if the folder exist
    check if the folder exist

--- LianJia/LianJia/getinfo.py
+++ LianJia/LianJia/getinfo.py
@@ -85,7 +85,17 @@ def get_house_url_current_district(district_url_list):
 
 # 把url写到本地
 def write_house_url(write_str, district):
-    path_file = "./data_url/house/" + district + ".txt"
+    local_path = './data_url/house'
+    # path_file = local_path + district + ".txt"
+    if not os.path.exists(local_path):
+    try:
+        os.mkdir(local_path)
+        print('Successfully created folder %s' % local_path)
+    except OSError:
+        print("Could not create directory")
+        sys.exit(1)
+        
+    path_file = local_path + district + ".txt"
     with open(path_file, 'w') as file:
         file.write(write_str)
 
