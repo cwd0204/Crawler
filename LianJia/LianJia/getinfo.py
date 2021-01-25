9a21bb6c239ba7c4edd5d02303391c134ec84369
cwd0204 <cwd0204@users.noreply.github.com>
2021-01-25 15:05:26 +0800

    x
    x

--- LianJia/LianJia/getinfo.py
+++ LianJia/LianJia/getinfo.py
@@ -88,13 +88,13 @@ def get_house_url_current_district(district_url_list):
 def write_house_url(write_str, district):
     local_path = './data_url/house'
     # path_file = local_path + district + ".txt"
-    if not os.path.exists(local_path):
-        try:
-            os.mkdir(local_path)
-            print('Successfully created folder %s' % local_path)
-        except OSError:
-            print("Could not create directory")
-            sys.exit(1)
+    # if not os.path.exists(local_path):
+    #     try:
+    #         os.mkdir(local_path)
+    #         print('Successfully created folder %s' % local_path)
+    #     except OSError:
+    #         print("Could not create directory")
+    #         sys.exit(1)
     path_file = local_path + district + ".txt"
     with open(path_file, 'w') as file:
         file.write(write_str)
